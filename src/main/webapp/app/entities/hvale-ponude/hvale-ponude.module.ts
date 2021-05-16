46df63ac8b7feb7c85235af4855b0a2b0bcb0972
drasko <drasko.kosovic@montefarm.co.me>
2021-05-16 22:10:03 +0200

    hvale ponude2

--- src/main/webapp/app/entities/hvale-ponude/hvale-ponude.module.ts
+++ src/main/webapp/app/entities/hvale-ponude/hvale-ponude.module.ts
@@ -4,9 +4,24 @@ import { SharedModule } from 'app/shared/shared.module';
 import { HvalePonudeComponent } from './list/hvale-ponude.component';
 import { HvalePonudeDetailComponent } from './detail/hvale-ponude-detail.component';
 import { HvalePonudeRoutingModule } from './route/hvale-ponude-routing.module';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableExporterModule } from 'mat-table-exporter';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  imports: [SharedModule, HvalePonudeRoutingModule],
+  imports: [
+    SharedModule,
+    HvalePonudeRoutingModule,
+    MatTableModule,
+    MatSortModule,
+    MatTableExporterModule,
+    MatPaginatorModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
   declarations: [HvalePonudeComponent, HvalePonudeDetailComponent],
 })
 export class HvalePonudeModule {}
--- src/main/webapp/app/entities/hvale-ponude/list/hvale-ponude.component.ts
+++ src/main/webapp/app/entities/hvale-ponude/list/hvale-ponude.component.ts
@@ -1,114 +1,67 @@
-import { Component, Inject, OnInit } from '@angular/core';
-import { HttpHeaders, HttpResponse } from '@angular/common/http';
-import { ActivatedRoute, Router } from '@angular/router';
-import { combineLatest } from 'rxjs';
-
+import { AfterViewInit, Component, Input, OnChanges, OnInit, ViewChild } from '@angular/core';
 import { IHvalePonude } from '../hvale-ponude.model';
 
-import { ITEMS_PER_PAGE } from 'app/config/pagination.constants';
 import { HvalePonudeService } from '../service/hvale-ponude.service';
-import { DOCUMENT } from '@angular/common';
-import { SERVER_API_URL } from 'app/app.constants';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'jhi-hvale-ponude',
   templateUrl: './hvale-ponude.component.html',
 })
-export class HvalePonudeComponent implements OnInit {
+export class HvalePonudeComponent implements OnInit, AfterViewInit, OnChanges {
   hvalePonudes?: any;
-  isLoading = false;
-  totalItems = 0;
-  itemsPerPage = ITEMS_PER_PAGE;
-  page?: number;
-  predicate!: string;
-  ascending!: boolean;
-  ngbPaginationPage = 1;
-  sifraPosupka? = 456;
-  public resourceUrlExel = SERVER_API_URL + 'api/excel-hvali/download/';
-  constructor(
-    protected hvalePonudeService: HvalePonudeService,
-    protected activatedRoute: ActivatedRoute,
-    protected router: Router,
-    @Inject(DOCUMENT) private document: Document
-  ) {}
+  public displayedColumns = [
+    'id',
+    'sifra postupka',
+    'broj partije',
+    'inn',
+    'farmaceutski oblik',
+    'pakovanje',
+    'kolicina',
+    'procijenjena vrijednost',
+  ];
+  public dataSource = new MatTableDataSource<IHvalePonude>();
 
-  public exel(): void {
-    this.document.location.href = this.resourceUrlExel + String(this.sifraPosupka);
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @Input() postupak: any;
+  constructor(protected hvaleService: HvalePonudeService) {}
+
+  public getSifraPostupka(): void {
+    this.hvaleService.hvali(this.postupak).subscribe((res: IHvalePonude[]) => {
+      this.dataSource.data = res;
+      // eslint-disable-next-line no-console
+      console.log(res);
+    });
   }
+
+  public getAllHvali(): void {
+    this.hvaleService.hvali(456).subscribe((res: IHvalePonude[]) => {
+      this.dataSource.data = res;
+      // eslint-disable-next-line no-console
+      console.log(res);
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  }
+  // getTotalCost(): any {
+  //   return this.ponudes?.map(t => t.ponudjenaVrijednost).reduce((acc, value) => acc! + value!, 0);
+  // }
+
+  public doFilter = (value: string): any => {
+    this.dataSource.filter = value.trim().toLocaleLowerCase();
+  };
+
+  ngOnChanges(): void {
+    this.getSifraPostupka();
+  }
+
   ngOnInit(): void {
-    this.getAllHvale();
-  }
-  loadPage(page?: number, dontNavigate?: boolean): void {
-    this.isLoading = true;
-    const pageToLoad: number = page ?? this.page ?? 1;
-
-    this.hvalePonudeService
-      .query({
-        page: pageToLoad - 1,
-        size: this.itemsPerPage,
-        sort: this.sort(),
-      })
-      .subscribe(
-        (res: HttpResponse<IHvalePonude[]>) => {
-          this.isLoading = false;
-          this.onSuccess(res.body, res.headers, pageToLoad, !dontNavigate);
-        },
-        () => {
-          this.isLoading = false;
-          this.onError();
-        }
-      );
-  }
-  public getAllHvale(): any {
-    this.hvalePonudeService.hvali(456).subscribe((res: any) => {
-      this.hvalePonudes = res;
-    });
-  }
-
-  trackId(index: number, item: IHvalePonude): number {
-    return item.id!;
-  }
-
-  protected sort(): string[] {
-    const result = [this.predicate + ',' + (this.ascending ? 'asc' : 'desc')];
-    if (this.predicate !== 'id') {
-      result.push('id');
-    }
-    return result;
-  }
-
-  protected handleNavigation(): void {
-    combineLatest([this.activatedRoute.data, this.activatedRoute.queryParamMap]).subscribe(([data, params]) => {
-      const page = params.get('page');
-      const pageNumber = page !== null ? +page : 1;
-      const sort = (params.get('sort') ?? data['defaultSort']).split(',');
-      const predicate = sort[0];
-      const ascending = sort[1] === 'asc';
-      if (pageNumber !== this.page || predicate !== this.predicate || ascending !== this.ascending) {
-        this.predicate = predicate;
-        this.ascending = ascending;
-        this.loadPage(pageNumber, true);
-      }
-    });
-  }
-
-  protected onSuccess(data: IHvalePonude[] | null, headers: HttpHeaders, page: number, navigate: boolean): void {
-    this.totalItems = Number(headers.get('X-Total-Count'));
-    this.page = page;
-    if (navigate) {
-      this.router.navigate(['/hvale-ponude'], {
-        queryParams: {
-          page: this.page,
-          size: this.itemsPerPage,
-          sort: this.predicate + ',' + (this.ascending ? 'asc' : 'desc'),
-        },
-      });
-    }
-    this.hvalePonudes = data ?? [];
-    this.ngbPaginationPage = this.page;
-  }
-
-  protected onError(): void {
-    this.ngbPaginationPage = this.page ?? 1;
+    this.getAllHvali();
   }
 }
