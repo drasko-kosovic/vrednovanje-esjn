000d72718612af7b91a4d4a007fd4201f3a54a3d
drasko <drasko.kosovic@montefarm.co.me>
2021-05-19 19:42:51 +0200

    mail

--- src/main/webapp/app/entities/postupci/list/postupci.component.ts
+++ src/main/webapp/app/entities/postupci/list/postupci.component.ts
@@ -17,7 +17,7 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 export class PostupciComponent implements OnInit, AfterViewInit {
   postupaks?: IPostupci[];
 
-  public displayedColumns = ['sifra postupka', 'opis postupka', 'vrsta postupka', 'datum objave', 'broj tendera', 'delete'];
+  public displayedColumns = ['sifra postupka', 'opis postupka', 'vrsta postupka', 'datum objave', 'broj tendera', 'delete', 'edit'];
   public dataSource = new MatTableDataSource<IPostupci>();
 
   @ViewChild(MatSort) sort!: MatSort;
