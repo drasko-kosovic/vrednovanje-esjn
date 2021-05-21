246db92de24f528fbc5e2b0a7f24caab54b494a0
drasko <drasko.kosovic@montefarm.co.me>
2021-05-21 10:13:37 +0200

    mail

--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -6,9 +6,10 @@ import { HOME_ROUTE } from './home.route';
 import { HomeComponent } from './home.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
-  imports: [SharedModule, RouterModule.forChild([HOME_ROUTE]), MatFormFieldModule, MatInputModule],
+  imports: [SharedModule, RouterModule.forChild([HOME_ROUTE]), MatFormFieldModule, MatInputModule, MatCardModule],
   declarations: [HomeComponent],
 })
 export class HomeModule {}
