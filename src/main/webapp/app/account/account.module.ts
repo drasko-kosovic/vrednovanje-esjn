04265572697390d56071e6307a7ea3224221e226
drasko <drasko.kosovic@montefarm.co.me>
2021-05-21 20:49:10 +0200

    mail

--- src/main/webapp/app/account/account.module.ts
+++ src/main/webapp/app/account/account.module.ts
@@ -10,9 +10,10 @@ import { PasswordResetInitComponent } from './password-reset/init/password-reset
 import { PasswordResetFinishComponent } from './password-reset/finish/password-reset-finish.component';
 import { SettingsComponent } from './settings/settings.component';
 import { accountState } from './account.route';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
-  imports: [SharedModule, RouterModule.forChild(accountState)],
+  imports: [SharedModule, RouterModule.forChild(accountState), MatCardModule],
   declarations: [
     ActivateComponent,
     RegisterComponent,
